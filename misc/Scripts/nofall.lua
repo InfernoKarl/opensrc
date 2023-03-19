99e060aecf60863f246d4f7114a17591ed868b4a
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2023-03-19 16:29:52 -0700

    no fall damage yayy

---
+++ misc/Scripts/nofall.lua
@@ -0,0 +1,29 @@
+local BlockedRemotes = {
+    "UpdateVelocity", 
+    "FallReport"
+}
+
+local Events = {
+    Fire = true, 
+    Invoke = true, 
+    FireServer = true, 
+    InvokeServer = true,
+}
+
+local gameMeta = getrawmetatable(game)
+local psuedoEnv = {
+    ["__index"] = gameMeta.__index,
+    ["__namecall"] = gameMeta.__namecall;
+}
+setreadonly(gameMeta, false)
+gameMeta.__index, gameMeta.__namecall = newcclosure(function(self, index, ...)
+    if Events[index] then
+        for i,v in pairs(BlockedRemotes) do
+            if v == self.Name and not checkcaller() then return nil end
+        end
+    end
+    return psuedoEnv.__index(self, index, ...)
+end)
+setreadonly(gameMeta, true)
+
+-- Credit to xchonkster on v3rm for the anti-remote firing, I just found the remotes to block
