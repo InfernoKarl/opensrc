4c17ae83de0ef5a705ebce404fbf741f265ad68e
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2023-03-24 23:18:10 -0700

    Infinite Attack, Defense, Health

---
+++ RPG-elevator/inf-atk-def-health.lua
@@ -0,0 +1,21 @@
+local toggle = false
+local player = game:GetService("Players").LocalPlayer
+local ohInstance1 = player.Items["A Beginner's Necklace"] -- Modify with whatever accessory you want to spam equip instead (if you already got rid of the starter necklace)
+
+local function toggleAccessory()
+    while toggle do
+        game:GetService("ReplicatedStorage").Remote.AccessoryPuEvent:FireServer(ohInstance1)
+        task.wait() 
+    end
+end
+
+local function keyPressed(input)
+    if input.KeyCode == Enum.KeyCode.T then
+        toggle = not toggle
+        if toggle then
+            toggleAccessory()
+        end
+    end
+end
+
+game:GetService("UserInputService").InputBegan:Connect(keyPressed)
