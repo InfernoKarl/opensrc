8777433d0ce0e6b13a8a8fa8ee5577be1ec2d87a
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2023-04-24 20:40:28 -0700

    Fixed Issue

--- ElementalGrindGame/Scripts/autoskill.lua
+++ ElementalGrindGame/Scripts/autoskill.lua
@@ -4,39 +4,6 @@ local backpack = player:WaitForChild("Backpack")
 
 local function checkTools()
     while true do
-        for _, tool in ipairs(backpack:GetChildren()) do
-            if tool:IsA("Tool") then
-                local toolName = tool.Name
-                local hasNumber = false
-
-                for i = 0, 9 do
-                    if string.find(toolName, tostring(i)) then
-                        hasNumber = true
-                        break
-                    end
-                end
-
-                if not hasNumber then
-                    player.Character.Humanoid:EquipTool(tool)
-                    tool:Activate()
-                    wait(0.1)
-                    tool:Deactivate()
-                    wait()
-                end
-            end
-        end
-        wait()
-    end
-end
-
-spawn(checkTools)
-
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-local backpack = player:WaitForChild("Backpack")
-
-local function checkTools()
-    while true do
         local foundToolWithoutNumber = false
         
         for _, tool in ipairs(backpack:GetChildren()) do
@@ -58,6 +25,7 @@ local function checkTools()
                     tool:Deactivate()
                     wait()
                     foundToolWithoutNumber = true
+                    player.Character.Humanoid:UnequipTools(tool)
                     break
                 end
             end
