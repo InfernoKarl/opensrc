1262dc533cf0b9c0457c7bb350395dfaf89c192f
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2023-04-24 20:27:34 -0700

    Auto Skill

---
+++ ElementalGrindGame/Scripts/autoskill.lua
@@ -0,0 +1,72 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local backpack = player:WaitForChild("Backpack")
+
+local function checkTools()
+    while true do
+        for _, tool in ipairs(backpack:GetChildren()) do
+            if tool:IsA("Tool") then
+                local toolName = tool.Name
+                local hasNumber = false
+
+                for i = 0, 9 do
+                    if string.find(toolName, tostring(i)) then
+                        hasNumber = true
+                        break
+                    end
+                end
+
+                if not hasNumber then
+                    player.Character.Humanoid:EquipTool(tool)
+                    tool:Activate()
+                    wait(0.1)
+                    tool:Deactivate()
+                    wait()
+                end
+            end
+        end
+        wait()
+    end
+end
+
+spawn(checkTools)
+
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local backpack = player:WaitForChild("Backpack")
+
+local function checkTools()
+    while true do
+        local foundToolWithoutNumber = false
+        
+        for _, tool in ipairs(backpack:GetChildren()) do
+            if tool:IsA("Tool") then
+                local toolName = tool.Name
+                local hasNumber = false
+
+                for i = 0, 9 do
+                    if string.find(toolName, tostring(i)) then
+                        hasNumber = true
+                        break
+                    end
+                end
+
+                if not hasNumber then
+                    player.Character.Humanoid:EquipTool(tool)
+                    tool:Activate()
+                    wait(0.1)
+                    tool:Deactivate()
+                    wait()
+                    foundToolWithoutNumber = true
+                    break
+                end
+            end
+        end
+        
+        if not foundToolWithoutNumber then
+            wait()
+        end
+    end
+end
+
+spawn(checkTools)
